3680ae16ae17db89cf648b8320b6e0b18d06b053
HamzaDazIA <hamzadaziag@gmail.com>
2025-12-05 13:48:55 +0100

    s

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,14 @@
+#ifndef BitcoinExchange_HPP
+#define BitcoinExchange_HPP
+
+#include <iostream>
+#include <map>
+
+class BitcoinExchange
+{
+    private : 
+        std::map<std::string , int> cont_map;
+        
+};
+
+#endif
